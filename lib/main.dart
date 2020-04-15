cf8516908aba510c7fcc5d718e8a484581825293
Prajwal S Belagavi <prajwalpspbelagavi@gmail.com>
2020-04-15 20:25:36 +0530

    theme change function

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
+import 'package:bloc_theme/global/theme/theme_bloc.dart';
+import 'package:bloc_theme/global/theme/theme_state.dart';
+import 'package:bloc_theme/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
+
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+    return BlocProvider(
+      builder: (context) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: _buildWithTheme
       ),
     );
   }
+
+  Widget _buildWithTheme(BuildContext context, ThemeState state) {
+        return MaterialApp(
+          title: 'Material App',
+          theme: state.themeData,
+          home: HomePage()
+        );
+      }
 }
